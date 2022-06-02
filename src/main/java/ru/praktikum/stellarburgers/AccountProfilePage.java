70d24d4adf27b8df6e7a683d8574749729a54a88
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-02 20:03:28 +0300

    added AccountProfilePage.java

---
+++ src/main/java/ru/praktikum/stellarburgers/AccountProfilePage.java
@@ -0,0 +1,19 @@
+package ru.praktikum.stellarburgers;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class AccountProfilePage extends CommonElements {
+    public static final String URL_PROF = MainPage.URL_BASE + "account/profile";
+
+    @FindBy(how = How.XPATH, using = ".//button[text()='Выход']")
+    private SelenideElement exitButton; // кнопка выход
+
+
+    @Step("Выйти из кабинета")
+    public void clickExitButton() {
+        exitButton.click();
+    }
+}
