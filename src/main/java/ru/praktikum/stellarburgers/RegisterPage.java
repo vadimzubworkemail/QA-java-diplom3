ab34b374ac53065d43be71152038367efb5c1cf0
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-02 19:38:09 +0300

    added RegisterPage.java

---
+++ src/main/java/ru/praktikum/stellarburgers/RegisterPage.java
@@ -0,0 +1,65 @@
+package ru.praktikum.stellarburgers;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class RegisterPage extends CommonElements {
+    public static final String URL_REG = MainPage.URL_BASE + "register";
+
+
+    @FindBy(how = How.XPATH, using = ".//fieldset[1]/div/div/input")
+    private SelenideElement nameField; // поле ввода имени
+
+    @FindBy(how = How.XPATH, using = ".//fieldset[2]/div/div/input")
+    private SelenideElement emailField; // поле ввода почты
+
+    @FindBy(how = How.XPATH, using = ".//fieldset[3]/div/div/input")
+    private SelenideElement passwordField; // поле ввода пароля
+
+    @FindBy(how = How.XPATH, using = ".//button[text()='Зарегистрироваться']")
+    private SelenideElement registerButton; // кнопка зарегистрироваться
+
+    @FindBy(how = How.XPATH, using = ".//a[text()='Войти']")
+    private SelenideElement logInButton; // кнопка войти
+
+    @FindBy(how = How.XPATH, using = ".//p[text()='Некорректный пароль']")
+    private SelenideElement passwordErrorMessage; // оповещение некорректный пароль
+
+
+    public void fillName(String name) {
+        nameField.setValue(name);
+    }
+
+    public void fillEmail(String email) {
+        emailField.setValue(email);
+    }
+
+    public void fillPassword(String password) {
+        passwordField.setValue(password);
+    }
+
+    public void clickRegisterButton() {
+        registerButton.click();
+    }
+
+    @Step("Регистрация пользователя")
+    public void registerNewUser(String name, String email, String password) {
+        fillName(name);
+        fillEmail(email);
+        fillPassword(password);
+        clickRegisterButton();
+    }
+
+    @Step("Кликнуть кнопку войти")
+    public void clickLogInButton() {
+        logInButton.click();
+    }
+
+    @Step("Получить оповещение системы о невалидном пароле")
+    public SelenideElement getInvalidPasswordErrorMessage() {
+        return passwordErrorMessage;
+    }
+}
+
