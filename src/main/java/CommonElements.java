9091759a9c13439299e76f69d9b7a0e26b429f52
vadimzubworkemail <vadim.zub.work.email@gmail.com>
2022-06-02 16:47:29 +0300

    added CommonElements.java

---
+++ src/main/java/CommonElements.java
@@ -0,0 +1,32 @@
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class CommonElements {
+    public final static String URL_BASE = "https://stellarburgers.nomoreparties.site/";
+
+    @FindBy(how = How.CLASS_NAME, using = "AppHeader_header__logo__2D0X2")
+    private SelenideElement logoButton; // кнопка логотип
+    @FindBy(how = How.XPATH, using = "//p[text()='Личный Кабинет']")
+    private SelenideElement personalAreaButton; // кнопка входа в личный кабинет
+    @FindBy(how = How.XPATH, using = "//p[text()='Конструктор']")
+    private SelenideElement constructorButton; // кнопка конструктор
+    @FindBy(how = How.CLASS_NAME, using = "undefined ml-2")
+    private SelenideElement orderFeedButton; // кнопка лента заказов
+
+    @Step("Клик на кнопку логотип")
+    public void clickLogoButton(){
+        logoButton.click();
+    }
+
+    @Step("Клик на кнопку личный кабинет")
+    public void clickPersonalAreaButton(){
+        personalAreaButton.click();
+    }
+
+    @Step("Клик на кнопку конструктор")
+    public void clickConstructorButton(){
+        constructorButton.click();
+    }
+}
